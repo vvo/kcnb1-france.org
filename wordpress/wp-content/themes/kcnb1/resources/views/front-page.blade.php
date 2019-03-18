58de7908043286fa7263f168acc554c67a6de54f
vvo <vincent.voyer@algolia.com>
2019-03-18 23:55:57 +0100

    updates

--- wordpress/wp-content/themes/kcnb1/resources/views/front-page.blade.php
+++ wordpress/wp-content/themes/kcnb1/resources/views/front-page.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.page-header')
-    @include('partials.content-page')
-  @endwhile
+  <div class="row">
+    <div class="col offset-md-1">
+      <img src="@asset('images/front-page/visuel.jpg')" alt="Photo de Soline a 8 ans" width="433" height="661" />
+    </div>
+    <div class="col pt-5">
+      <h1 class="headline mt-4">La mutation du gène KCNB1 est une maladie génétique rare.</h1>
+    </div>
+  </div>
 @endsection
