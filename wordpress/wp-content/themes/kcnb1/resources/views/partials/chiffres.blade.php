f82cfa10e28a158a8100ae0c0adfa8c5e5f23577
vvo <vincent.voyer@algolia.com>
2019-05-08 01:07:07 +0200

    updates

---
+++ wordpress/wp-content/themes/kcnb1/resources/views/partials/chiffres.blade.php
@@ -0,0 +1,42 @@
+<div class="bg-blue pt-5 text-white pb-5">
+  <div class="container">
+      <h2 class="text-center">L'association KCNB1 France <br/>en quelques chiffres</h2>
+      <div class="row">
+        <div class="col mt-5">
+          <div class="mx-auto number d-flex align-items-center justify-content-center">
+            <div>
+              <div class="numberTitle">14</div>
+              <div class="numberSubtitle">Patients diagnostiqués</div>
+            </div>
+          </div>
+        </div>
+        <div class="col mt-5">
+          <div class="mx-auto number d-flex align-items-center justify-content-center">
+            <div>
+              <div class="numberTitle">50</div>
+              <div class="numberSubtitle">50 000 euros de dons</div>
+            </div>
+          </div>
+        </div>
+        <div class="col mt-5">
+          <div class="mx-auto number d-flex align-items-center justify-content-center">
+            <div>
+              <div class="numberTitle">305</div>
+              <div class="numberSubtitle">Donateurs</div>
+            </div>
+          </div>
+        </div>
+        <div class="col mt-5">
+          <div class="mx-auto number d-flex align-items-center justify-content-center">
+            <div>
+              <div class="numberTitle">45</div>
+              <div class="numberSubtitle">Bénévoles</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p class="text-center mt-5">
+          <a href="test" class="btn btn-red btn-lg mt-2">Faire un don à l'association</a>
+      </p>
+  </div>
+</div>
