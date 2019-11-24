8dcb62801f339f3032bc49700b87e62d6a9dad52
Vincent Voyer <vincent@codeagain.com>
2019-11-24 22:33:46 +0100

    mise a jour dons

--- wordpress/wp-content/themes/kcnb1/resources/views/page-faire-un-don.blade.php
+++ wordpress/wp-content/themes/kcnb1/resources/views/page-faire-un-don.blade.php
@@ -25,6 +25,9 @@
             </div>
           </div>
           <div class="col-md border-left pl-4">
+            <p>
+              L'association KCNB1 France est une association loi 1901. Votre don ouvre droit à une réduction fiscale car il remplit les conditions générales prévues aux articles 200, 238 bis du code général des impôts (CGI). Si vous êtes un particulier, vous pouvez déduire 66% de votre don. Soit <strong>pour un don de 50 €, celui-ci vous coûtera en réalité 17 € après réduction fiscale</strong>.
+            </p>
             <p>Vos dons sont utilisés pour : <strong>la recherche</strong> avec l'institut <a href="https://www.institutimagine.org/fr/" title="Lien vers le site de l'institut de recherche Imagine">Imagine</a>, un institut de recherche et de soins public privé, l'<strong>organisation d'événements</strong> par l'association et <strong>tout autre projet nécessaire</strong> <a href="{{ get_permalink(24) }}">au combat des familles</a> contre les maladies liées à la mutation du gène KCNB1.</p>
             <p>Depuis décembre 2017 <a href="{{ get_permalink(25) }}">un programme de recherche</a> clinique et fondamentale sur la mutation du gène KCNB1 a été lancé à l'<a href="http://hopital-necker.aphp.fr/">hopital Necker-Enfants malades</a> et l'institut Imagine.</p>
           </div>
@@ -34,14 +37,17 @@
       <button class="btn btn-outline-black mt-2" type="button" data-toggle="collapse" data-target="#don-cheque" aria-expanded="true" aria-controls="don-cheque">
         Par chèque
       </button>
-      <div id="don-cheque" class="collapse row p-4 border-bottom">
-        <p class="border-bottom col-md">
-          À l'ordre de : <u>Association KCNB1 France</u> et à envoyer à : <br/><br>
+      <div id="don-cheque" class="collapse row p-4">
+        <p class="col-md">
+          À l'ordre de : <u>Association KCNB1 France</u> à envoyer à : <br/><br>
+
           <span class="text-green">
             Association KCNB1 France
             <br/>6 rue des Martins
             <br/>44230 Saint-Sébastien
           </span>
+
+          Indiquez-nous dans votre envoi votre email ou adresse postale afin que nous puissions vous fournir votre reçu fiscal.
           </p>
       </div>
 
